f5aefe2a4e487cf21b2690c7d3a94b3c5bb9cf42
Pulkit <spulkit489@gmail.com>
2018-06-17 13:45:21 +0530

    half done

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -59,6 +59,8 @@ Instructions:
     Your code goes here! Uncomment the next line when you're ready to start!
      */
 
-    // getJSON('../data/earth-like-results.json')
+    getJSON('../data/earth-like-results.json').then(function(response){
+      // To enter tHsi
+    });
   });
 })(document);
